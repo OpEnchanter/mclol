c8290bb535623bb187e339b159697725110fe9c4
OpEnchanter <openchanter@gmail.com>
2024-10-31 23:04:35 -0400

    started work on admin panel

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, Response
 
 app = Flask(__name__)
 
@@ -9,6 +9,25 @@ def home():
 @app.route('/wiki')
 def wiki():
     return render_template("wiki/index.html")
+
+@app.route('/api/admin', methods=['GET'])
+def admin():
+    
+    responseHtml = ""
+
+    with open("lockedPages/admin.html", 'r') as html:
+        responseHtml = html.read()
+
+    path = request.path
+    reqData = request.get_json
+
+    if (path == "/api/admin"):
+
+        return Response(responseHtml, content_type="text/html")
+    
+        
+
+    return(jsonify("Invalid request path"))
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
