b70a1a613b5c33ff72e1057f2122d12de85556f0
OpEnchanter <openchanter@gmail.com>
2024-11-02 17:12:00 -0400

    Added admin command through browser

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request, jsonify, Response
+import random
+from mcrcon import MCRcon
 
 app = Flask(__name__)
+
+rconIp = "127.0.0.1"
+rconPort = 25566
+rconPassword = "rcon"
+
+mcr = None
+
+sessionKeys = []
+
+class session():
+    def __init__(self, key):
+        self.key = key
+
+
+def create_session():
+    key = random.randint(1111111111, 9999999999)
+    sessionKeys.append(key)
+    return session(key)
 
 @app.route('/')
 def home():
@@ -30,15 +50,40 @@ def api_admin():
         return jsonify({"error": "Invalid JSON data"}), 400
     
     if reqData["key"] == "test":
+        session = create_session()
+
         return jsonify({
             "response": True,
+            "skey": session.key,
             "html": responseHtml
         })
 
     return jsonify({
         "response": False
     })
-    
+
+@app.route('/api/command', methods=['POST'])
+def api_command():
+    global mcr
+    reqData = request.get_json()
+
+    print(reqData)
+
+    if reqData["skey"] in sessionKeys:
+
+        if mcr is None:
+            mcr = MCRcon(rconIp, rconPassword, rconPort)
+            mcr.connect()
+
+        result = mcr.command(reqData["command"])
+        return jsonify({
+            "response": True,
+            "result": result
+        })
+
+    return jsonify({
+        "response": False
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
